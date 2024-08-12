3cb443f69808fb5738f106c0211f92fc51a3786f
TimmySK <Karanvirkooner123@outlook.com>
2024-08-12 16:51:28 -0400

    done

--- Class_Notes/HTML/Assignment_4/Part_2/main.js
+++ Class_Notes/HTML/Assignment_4/Part_2/main.js
@@ -7,22 +7,27 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 
 const imageFilenames = [
-    'pic1.jpg',
-    'pic2.jpg',
-    'pic3.jpg',
-    'pic4.jpg',
-    'pic5.jpg',
+    'images/pic1.jpg',
+    'images/pic2.jpg',
+    'images/pic3.jpg',
+    'images/pic4.jpg',
+    'images/pic5.jpg',
 ];
 
 /* Declaring the alternative text for each image file */
 
 const altTexts = {
-    'pic1.jpg': 'Human eye',
-    'pic2.jpg': 'Abstract pattern painting',
-    'pic3.jpg': 'violet and white flowers',
-    'pic4.jpg': 'Egyptian Painting',
-    'pic5.jpg': 'Butterfly on a leaf',
+    'images/pic1.jpg': 'Human eye',
+    'images/pic2.jpg': 'Abstract pattern painting',
+    'images/pic3.jpg': 'violet and white flowers',
+    'images/pic4.jpg': 'Egyptian Painting',
+    'images/pic5.jpg': 'Butterfly on a leaf',
 };
+
+function handleImageClick({ target }) {
+    displayedImage.setAttribute('src', target.getAttribute('src'));
+    displayedImage.setAttribute('alt', target.getAttribute('alt'));
+}
 
 /* Looping through images */
 
@@ -31,6 +36,7 @@ imageFilenames.forEach((imageName) => {
     newImage.setAttribute('src', `images/${imageName}`);
     newImage.setAttribute('alt', altTexts[imageName]);
     thumbBar.appendChild(newImage);
+    img.addEventListener('click', handleImageClick);
 });
 
 const newImage = document.createElement('img');
@@ -39,3 +45,17 @@ newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
+function handleButtonClick() {
+    let currentClass = btn.getAttribute('class');
+    if (currentClass === 'dark') {
+        btn.textContent = 'lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+        btn.setAttribute('class', 'light');
+    } else {
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        btn.setAttribute('class', 'dark');
+    }
+}
+
+btn.addEventListener('click', handleButtonClick);
