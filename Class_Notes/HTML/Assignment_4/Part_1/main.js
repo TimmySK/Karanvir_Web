3622023f5a9d89008931e2734d6a0e045d22660e
TimmySK <Karanvirkooner123@outlook.com>
2024-08-12 16:22:18 -0400

    Done part_1

--- Class_Notes/HTML/Assignment_4/Part_1/main.js
+++ Class_Notes/HTML/Assignment_4/Part_1/main.js
@@ -21,17 +21,19 @@ function result() {
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
 
+    newStory = newStory.replaceAll(":insertx:", xItem).replace(":inserty:", yItem).replace(":insertz:", zItem);
+
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace("bob", name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature =  `${Math.round((94 - 32) * (5 / 9))} centigrade`;
+    newStory = newStory.replace("300 pounds", weight).replace("94 fahrenheit", temperature);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
