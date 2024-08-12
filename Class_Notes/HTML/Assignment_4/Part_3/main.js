39efd886576a3238b03040011d691acd3034186e
TimmySK <Karanvirkooner123@outlook.com>
2024-08-12 17:24:45 -0400

    4th commit

--- Class_Notes/HTML/Assignment_4/Part_3/main.js
+++ Class_Notes/HTML/Assignment_4/Part_3/main.js
@@ -9,48 +9,76 @@ const height = (canvas.height = window.innerHeight);
 // function to generate random number
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 
 function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
 }
-
+  
 Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, thix.size, 0, 2 * Math.PI);
-  ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
 }
 
 Ball.prototype.update = function() {
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
   }
 
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
+Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        }
+      }
+    }
+  }  
 
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
+let balls = [];
 
-  if ((this.y - this.size) <=0) {
-    this.velY = -(this.velY);
-  }
+while (balls.length < 35) {
+  let size = random(20,30);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
 
-  this.x += this.velX;
-  this.y += this.velY;
+  balls.push(ball);
 }
+
